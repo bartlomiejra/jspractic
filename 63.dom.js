ec4c2858b1c4dc170d5571769bc94dd249f33c2c
bartlomiejra <bartlomiej.ra@gmail.com>
2020-01-18 14:19:57 +0100

    Add files via upload

---
+++ 63.dom.js
@@ -0,0 +1,10 @@
+//JS window document object
+
+/*
+  -window
+    -object
+
+*/
+//window.document.write("<br> document object is property of window paragraph.")
+
+document.write("<br> document object is property of window paragraph.")
