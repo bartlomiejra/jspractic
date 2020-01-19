5cb8a540acab66c95ac519d36b170d52354ddde5
bartlomiejra <bartlomiej.ra@gmail.com>
2020-01-19 20:22:08 +0100

    Add files via upload

--- myscript.js
+++ myscript.js
@@ -1,56 +1,25 @@
-//JS _ Accesing elements & attributes using document object
-
-var hElement = document.getElementById("my-heading")
-// // access an element using get ElementbyID
-// var hElement = document.getElementById("my-heading");
+//Navigating between elements
+var ulElements = document.getElementsByTagName("ul")[0];
+// alert("Node name: " + ulElements.nodeName);
 //
-//  alert(hElement);
+// alert("Node type: " + ulElements.nodeType);
 //
-// //get DOM object value
-//  alert(hElement.innerHTML);
+// // get node perent
+// alert("node perent: " + ulElements.parentNode.nodeName);
 //
-// //get DOM object attribute value
-//  alert(hElement.getAttribute("id"));
+// // get child nodes
 //
-//  //get tag name
-//  alert(hElment.tagName);
- var pElements = document.getElementsByClassName("my-paragraph");
+// alert("Node child Nodes: " + ulElements.childNodes);
+//
+// //get child nodes
+// alert("Child Nodes length: " + ulElements.childNodes.length
+//  )
 
-// alert(pElements);
-// alert(pElements[0]);
-// alert(pElements[0].innerHTML);
- //loop through all paragraphs
- for ( var i=0; i < pElements.length; i++){
+var ulChildNodes = ulElements.childNodes;
+for ( var i = 0; i < ulChildNodes.langth; i++) {
 
-   //alert(pElements[i].innerHTML);
- }
+  if ( ulChildNodes[i].nodeType == 1 ) {
 
-//access elements using tag Name
-var ulElements = document.getElementsByTagName("ul");
-
-// alert(ulElements);
-// alert(ulElements[1].innerHTML);
-
-var liElements= ulElements[1].getElementsByTagName("li");
-
-// alert(liElements [0].innerHtml);
-
-//access elements using name attribute
-
-var naElements = document.getElementsByName("user-name");
-
-alert(naElements[0].tagName);
-
-//accrees element ussing css query
-var pElement = document.querySelector("p");
-alert (pElement.innerHTML);
-
-var hElement = document.querySelector("#my-heading");
-//alert(hElement.innerHTML);
-
-var pElements = document.querySelectorAll("p");
-//alert(pElements[1].innerHTML);
-
-
-var pElements = document.querySelectorAll("div p");
-alert(pElements[1].innerHTML);
+    alert(ulChildNodes[i].innerHTML);
+  }
+}
