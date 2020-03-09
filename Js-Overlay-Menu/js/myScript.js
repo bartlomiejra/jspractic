a1a66da9be2f5ee1d60f9c322acaaa724191f470
bartlomiejra <bartlomiej.ra@gmail.com>
2020-03-09 01:35:14 +0100

    Add files via upload

--- Js-Overlay-Menu/js/myScript.js
+++ Js-Overlay-Menu/js/myScript.js
@@ -1,1 +1,37 @@
+var btnMenu = document.getElementById('btn-menu');
+btnMenu.addEventListener('click', toggleMenu);
 
+function toggleMenu(e) {
+  var divOverlay = document.getElementsByClassName('overlay')[0];
+  var speed = 10;
+  var eventTarget = e.target;
+  if (eventTarget.className == 'btn-open') {
+    fadeIn(divOverlay, speed);
+    eventTarget.className = 'btn-close';
+  } else if (eventTarget.className == 'btn-close') {
+    fadeOut(divOverlay, speed);
+    eventTarget.className = 'btn-open';
+  }
+}
+
+function fadeIn(elem, speed) {
+  var inInterval = setInterval(function() {
+    elem.style.opacity = Number(elem.style.opacity) + 0.01;
+
+    if (elem.style.opacity >= 1) {
+      elem.style.opacity = 1;
+      clearInterval(inInterval);
+    }
+  }, speed); //10 ms ==01s
+}
+
+function fadeOut(elem, speed) {
+  var outInterval = setInterval(function() {
+    elem.style.opacity = Number(elem.style.opacity) - 0.01;
+
+    if (elem.style.opacity <= 0) {
+      elem.style.opacity = 0;
+      clearInterval(outInterval);
+    }
+  }, speed); //10 ms ==01s
+}
